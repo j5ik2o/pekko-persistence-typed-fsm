4ca5c30f90cc5142943505afd656a7ff4699d4ee
Junichi Kato <j5ik2o@gmail.com>
2025-04-20 09:40:45 +0900

    refactor: format output statement for better readability in HelloWorldScala

--- example/src/main/scala/com/github/j5ik2o/pekko/persistence/effector/example/HelloWorldScala.scala
+++ example/src/main/scala/com/github/j5ik2o/pekko/persistence/effector/example/HelloWorldScala.scala
@@ -5,6 +5,7 @@ import com.github.j5ik2o.pekko.persistence.effector.scaladsl.PersistenceEffector
 object HelloWorldScala {
   def main(args: Array[String]): Unit = {
     println("Hello, World! from Scala")
-    println(s"Using PersistenceEffector library version: ${PersistenceEffector.getClass.getPackage.getImplementationVersion}")
+    println(
+      s"Using PersistenceEffector library version: ${PersistenceEffector.getClass.getPackage.getImplementationVersion}")
   }
 }
